11b0032eb54b98848301eb2f2ba786fccf1bd670
Longarithm <the.alex.logunov@gmail.com>
2024-11-09 10:27:53 +0100

    >

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
-use std::io::BufRead;
+use std::io::{BufRead, IsTerminal, Write};
 use std::process::Command;
+
+fn read_line(stdin: &mut dyn BufRead) -> Result<String, std::io::Error> {
+    let mut line = String::new();
+    stdin.read_line(&mut line)?;
+    Ok(line)
+}
 
 fn parse_command(cmd: &str) -> Result<Command, String> {
     let tokens = cmd.split_whitespace().collect::<Vec<&str>>();
@@ -20,17 +26,21 @@ fn parse_commands(cmd: &str) -> Result<Vec<Command>, String> {
 }
 
 fn main() {
-    let stdin = std::io::stdin();
-    let mut lines = stdin.lock().lines();
+    let mut stdin = std::io::stdin().lock();
+    let mut stdout = std::io::stdout();
     loop {
-        let maybe_cmd = lines.next();
+        if stdout.is_terminal() {
+            print!("{}", "> ");
+            stdout.flush().unwrap();
+        }
+
+        let maybe_cmd = read_line(&mut stdin);
         let cmd = match maybe_cmd {
-            Some(Ok(cmd)) => cmd,
-            Some(Err(e)) => {
+            Ok(cmd) => cmd,
+            Err(e) => {
                 println!("Error: {}", e);
                 return;
             }
-            None => return,
         };
 
         let commands = parse_commands(&cmd).unwrap();
