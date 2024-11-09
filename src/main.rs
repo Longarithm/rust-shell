38690146388602225cce1aadb7343fdaef91e360
Longarithm <the.alex.logunov@gmail.com>
2024-11-09 12:22:00 +0100

    cd, exit

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use std::ffi::OsStr;
 use std::io::{BufRead, IsTerminal, Write};
 use std::process::Command;
+
+// TODOs
+// AND doesn't work because it doesn't terminate if false.
+// OR doesn't work. In fact everything is just executed in a row.
+// Quotes don't work. Like, "&&" is treated as &&
 
 fn read_line(stdin: &mut dyn BufRead) -> Result<String, std::io::Error> {
     let mut line = String::new();
@@ -7,52 +13,36 @@ fn read_line(stdin: &mut dyn BufRead) -> Result<String, std::io::Error> {
     Ok(line)
 }
 
-// enum Token<'a> {
-//     And,
-//     Or,
-//     Text(&'a str),
-// }
-
-// fn parse_token(token: &str) -> Result<Token, std::io::Error> {
-//     match token {
-//         "&&" => Ok(Token::And),
-//         "||" => Ok(Token::Or),
-//         _ => Ok(Token::Text(token)),
-//     }
-// }
-
-// fn parse_separated(tokens: Vec<Token>) -> Result<Vec<Command>, std::io::Error> {
-//     let mut commands = Vec::new();
-
-//     for token in tokens {
-//         match token {
-//             Token::Text(text) => {
-//                 commands.push(command);
-//             }
-//             _ => {}
-//         }
-//     }
-//     Ok(commands)
-// }
-
-// let raw_tokens = cmd.split_whitespace().collect::<Vec<&str>>();
-// let (cmd, args) = token.split_once(' ').unwrap();
-// let mut command = Command::new(cmd);
-// command.args(args);
-
-fn parse_command(cmd: &str) -> Result<Command, std::io::Error> {
+fn parse_command(cmd: &str) -> Result<Option<Command>, std::io::Error> {
     let raw_tokens = cmd.split_whitespace().collect::<Vec<&str>>();
+    if raw_tokens.is_empty() {
+        return Ok(None);
+    }
     let (cmd, args) = raw_tokens.split_first().unwrap();
     let mut command = Command::new(cmd);
     command.args(args);
-    Ok(command)
+    Ok(Some(command))
 }
 
-fn parse_by_ampersands(cmd: &str) -> Result<Vec<Command>, std::io::Error> {
+#[allow(unused)]
+fn parse_by_or(cmd: &str) -> Result<Vec<Command>, std::io::Error> {
+    let subcommands = cmd.split("||").collect::<Vec<&str>>();
+    let commands = subcommands
+        .iter()
+        .map(|subcmd| parse_command(subcmd))
+        .flatten()
+        .flatten()
+        .collect();
+    Ok(commands)
+}
+
+fn parse_by_and(cmd: &str) -> Result<Vec<Command>, std::io::Error> {
     let subcommands = cmd.split("&&").collect::<Vec<&str>>();
     let commands = subcommands
         .iter()
-        .map(|subcmd| parse_command(subcmd).unwrap())
+        .map(|subcmd| parse_command(subcmd))
+        .flatten()
+        .flatten()
         .collect();
     Ok(commands)
 }
@@ -62,35 +52,55 @@ fn parse_commands(cmd: &str) -> Result<Vec<Command>, std::io::Error> {
     let subcommands = cmd.split(';').collect::<Vec<&str>>();
     let commands = subcommands
         .iter()
-        .map(|cmd| parse_by_ampersands(cmd).unwrap())
+        .map(|cmd| parse_by_and(cmd).unwrap())
         .flatten()
         .collect();
     Ok(commands)
 }
 
+fn execute_cd(command: Command) {
+    let dir = command.get_args().next().unwrap();
+    match std::env::set_current_dir(dir) {
+        Ok(_) => (),
+        Err(e) => {
+            eprintln!("Error: {}", e);
+            return;
+        }
+    }
+}
+
 fn main() {
     let mut stdin = std::io::stdin().lock();
     let mut stdout = std::io::stdout();
+
     loop {
         if stdout.is_terminal() {
             print!("{}", "> ");
-            stdout.flush().unwrap();
         }
+        stdout.flush().unwrap();
 
         let maybe_cmd = read_line(&mut stdin);
         let cmd = match maybe_cmd {
             Ok(cmd) => cmd,
             Err(e) => {
-                println!("Error: {}", e);
+                eprintln!("Error: {}", e);
                 return;
             }
         };
 
         let commands = parse_commands(&cmd).unwrap();
         for mut command in commands {
-            match command.output() {
-                Ok(output) => print!("{}", String::from_utf8(output.stdout).unwrap()),
-                Err(e) => println!("Error: {}", e),
+            match command.get_program().to_str().unwrap() {
+                "cd" => {
+                    execute_cd(command);
+                }
+                "exit" => {
+                    return;
+                }
+                _ => match command.output() {
+                    Ok(output) => print!("{}", String::from_utf8(output.stdout).unwrap()),
+                    Err(e) => eprintln!("Error: {}", e),
+                },
             }
         }
     }
