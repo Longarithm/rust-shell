3afdbcfa8597b9adae62fa99689d9a02f5f95153
Longarithm <the.alex.logunov@gmail.com>
2024-11-09 10:21:37 +0100

    terminator - 2

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 use std::io::BufRead;
 use std::process::Command;
 
+fn parse_command(cmd: &str) -> Result<Command, String> {
+    let tokens = cmd.split_whitespace().collect::<Vec<&str>>();
+    let (cmd, args) = tokens.split_first().unwrap();
+    let mut command = Command::new(cmd);
+    command.args(args);
+    Ok(command)
+}
+
+fn parse_commands(cmd: &str) -> Result<Vec<Command>, String> {
+    // Split terminator
+    let raw_commands = cmd.split(';').collect::<Vec<&str>>();
+    let commands = raw_commands
+        .iter()
+        .map(|cmd| parse_command(cmd).unwrap())
+        .collect();
+    Ok(commands)
+}
+
 fn main() {
     let stdin = std::io::stdin();
     let mut lines = stdin.lock().lines();
@@ -15,14 +33,12 @@ fn main() {
             None => return,
         };
 
-        let tokens = cmd.split_whitespace().collect::<Vec<&str>>();
-        let (cmd, args) = tokens.split_first().unwrap();
-
-        let output = Command::new(cmd).args(args).output();
-
-        match output {
-            Ok(output) => print!("{}", String::from_utf8(output.stdout).unwrap()),
-            Err(e) => println!("Error: {}", e),
+        let commands = parse_commands(&cmd).unwrap();
+        for mut command in commands {
+            match command.output() {
+                Ok(output) => print!("{}", String::from_utf8(output.stdout).unwrap()),
+                Err(e) => println!("Error: {}", e),
+            }
         }
     }
 }
